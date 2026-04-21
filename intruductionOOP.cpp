6e391bd5fcc62b281172893dc30b45ba7762263c
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-21 17:40:32 +0700

    menambahkan object

--- intruductionOOP.cpp
+++ intruductionOOP.cpp
@@ -13,3 +13,12 @@ public :
         cout << "Nilai : " << nilai << endl;
     }
 }; //batas class
+
+int main(){
+    Mahasiswa mhs; //object
+    mhs.nim = 2022;
+    mhs.nama = "Abra";
+    mhs.nilai = 90.5;
+
+    
+}
